bd1c428372f55eeb02161fac0ae21b9f34e4653e
carlos-puerto-garcia <cpuertog@gmail.com>
2018-09-18 19:29:46 -0500

    ejemplo.py

---
+++ ejemplo.py
@@ -0,0 +1,11 @@
+
+def suma (x,y):
+    return x + y
+'''
+numero1=int(input("Ingrese un número entero \n"))
+numero2=int(input("ingrese otro número entero"))
+
+suma= (numero1 + numero2)
+
+print ("La suma de los dos números enteros es",suma)
+'''

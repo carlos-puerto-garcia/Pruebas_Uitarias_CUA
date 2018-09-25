ffc951a34b36c769f85decd8aab3c5830baa2ef1
suarez88a <suarezcarlos6103@coruniamericana.edu.co>
2018-09-25 13:28:00 -0500

    Actualizar función Andres Suarez

---
+++ Andres_Suarez.py
@@ -0,0 +1,2 @@
+def promedio (nota1, nota2, nota3, nota4):
+    return (nota1+nota2+nota3+nota4) / 4
--- pruebas_unitarias_con_python.py
+++ pruebas_unitarias_con_python.py
@@ -5,13 +5,12 @@ import SEBASTIAN_YEPES
 import factorialDiegoHerrera
 import JOHAN_ALVAREZ
 import JOHAN_LOPEZ
+import Andres_Suarez
 import unittest
-
 
 '''
 creamos los casos de prueba para las funciones
 y para eso debemos crear una clase
-
 '''
 
 class Test_ejemplo_suma (unittest.TestCase):
@@ -20,7 +19,7 @@ class Test_ejemplo_suma (unittest.TestCase):
         result=ejemplo.suma(7,3)
         self.assertEqual(result,10)
 
-# Despues de este mensaje ustedes deben integrar sus casos de prueba
+#Despues de este mensaje ustedes deben integrar sus casos de prueba
 
 class TestOmarSalgado (unittest.TestCase):
     #Escribimos un metodo:
@@ -34,44 +33,37 @@ class Test_Manuel_Buritica (unittest.TestCase):
         result= ManuelBuritica.entrada(21, 50000)
         self.assertEqual(result,1)
 
-
-
-#clase sebastian yepes pares e impares:
-
 class Test_ejemplo_sebastian (unittest.TestCase):
-    
+    #Escribimos un metodo:
     def test_par_impar (self):
         result=SEBASTIAN_YEPES.par_impares(2)
         self.assertEqual(result,1)
-
         
 class testDiegoHerrera (unittest.TestCase):
     #Escribimos un metodo:
     def testfactorial(self):
         result= factorialDiegoHerrera.fact(4)
         self.assertEqual(result,24)
-        
-if __name__ == '__main__':
-    unittest.main()
 
-#clase johan alvarez
+#clase Johan Alvarez
 class Test_ejemplo_triangulo (unittest.TestCase):
     #Escribimos un metodo:
     def test_area_triangulo (self):
         result=JOHAN_ALVAREZ.area_triangulo(2,2)
         self.assertEqual(result,2)
 
-if __name__ == '__main__':
-    unittest.main()
-
-
-#clase johan lopez
-
+#clase Johan Lopez
 class Test_ejemplo_calcular (unittest.TestCase):
     #Escribimos un metodo:
     def test_calcular (self):
         result=JOHAN_LOPEZ.calcular(2000,2000)
-        self.assertEqual(result,40000)
+        self.assertEqual(result,-38000)
+
+class Test_Andres_Suarez (unittest.TestCase):
+    #Escribimos un metodo:
+    def testPromedio (self):
+        result= Andres_Suarez.promedio(5,4.5,3.5,1)
+        self.assertEqual(result,3.5)
 
 if __name__ == '__main__':
     unittest.main()
